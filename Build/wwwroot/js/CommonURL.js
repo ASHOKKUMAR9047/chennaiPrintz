5146b700927adc6546ff7bddde3a337a9c5d3e61
ASHOKKUMAR K <80088460+ASHOKKUMAR9047@users.noreply.github.com>
2024-11-28 20:16:50 +0530

    LastUpdateJobcard

--- Build/wwwroot/js/CommonURL.js
+++ Build/wwwroot/js/CommonURL.js
@@ -1,5 +1,5 @@
 ﻿/*var gStrIpVal = "http://192.168.0.107:7122/";*/
 
-/*var gStrIpVal = "http://192.168.0.124:7127/";*/
+//var gStrIpVal = "http://192.168.0.135:7127/";
 
 var gStrIpVal = "http://192.168.29.204:5555/";
